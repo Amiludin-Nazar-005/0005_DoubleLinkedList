dbf4729c5a67c0bcd210171f31c5d46d4bafa9de
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-05-15 10:32:48 +0700

    membuat class double linkes list

--- duoblelinkedlist.cpp
+++ duoblelinkedlist.cpp
@@ -4,7 +4,25 @@ using namespace std;
 
 class node{
     public:
-    int nomMhs;
+    int noMhs;
     node *next;
     node *prev;
+};
+
+class doublelinkedlist{
+    private:
+    node *START;
+
+    public:
+    doublelinkedlist(){
+        START = NULL;
+    }
+
+    void addnode(){
+        int nim;
+        string nim;
+        cout << "\nEnter the roll number of the student";
+        cin >> nim;
+    }
+
 };
