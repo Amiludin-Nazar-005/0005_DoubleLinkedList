1c0927eb00a9e7255881e396be4993af75bbdadb
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-05-15 12:18:30 +0700

    membuat choice

--- duoblelinkedlist.cpp
+++ duoblelinkedlist.cpp
@@ -185,5 +185,33 @@ int main()
         cout << "enter your choice:";
 
         cin >> choice;
-    }
+        switch (choice)
+        {
+        case '1':
+            list.addnode();
+            break;
+        case '2':
+            list.hapus();
+            break;
+        case '3':
+            list.traverse();
+            break;
+        case '4':
+            list.retraverse();
+            break;
+        case '5':
+            list.searchdata();
+            break;
+        case '6':
+            return 0;
+        default:
+            cout << "invalid option\n";
+        }
+        cout << "\nPress enter to continue...";
+        cin.ignore();
+        cin.get();
+        cout << endl;
+        system("clear");
+       
+    } while(choice != '6');
 }
