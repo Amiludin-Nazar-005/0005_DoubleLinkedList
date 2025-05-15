7311aa5868694e7b27a05139d36f39d78fe0ee14
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-05-15 11:11:52 +0700

    add step 5

--- duoblelinkedlist.cpp
+++ duoblelinkedlist.cpp
@@ -29,20 +29,41 @@ public:
         cin >> nim;
 
         // step 1: allcate memory for new node
-    node *newNode = new node();
+        node *newNode = new node();
 
-    // step 2 : assign value to the data fields
-    newNode->noMhs = nim;
+        // step 2 : assign value to the data fields
+        newNode->noMhs = nim;
 
-    // step 3: insert st beginning
-    if (START == NULL || nim <= START->noMhs){
-        if (START != NULL && nim == START->noMhs){
-            cout << "\nDuplicate number not allowed "<< endl;
+        // step 3: insert st beginning
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed " << endl;
+                return;
+            }
+            // step 4: newnode.next = start
+            newNode->next = START;
+
+            // step 5 : start . prev
+            if (START != NULL)
+                START->prev = NULL;
+
+            newNode->prev = NULL
+
+                START = newNode;
             return;
         }
-    }
-    // step 4: newnode.next = start
-    newNode->next = START;
-    }
+        node * current = START;
+        while (current-> next != NULL && current -> next-> noMhs < nim ){
+            current = current -> next;
+        }
+        if (current -> next != NULL && nim == current ->next->noMhs){
+            cout << "\nDuplicate roll number not allowed"<< endl;
+            return;
+        }
 
+        newNode->next = current -> next;
+        newNode-> prev = current;
+    }
 };
